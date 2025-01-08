cdbd364a8093faab23b3955660dcf8c7767ac0fe
onwaree <onwareesrimaya21082545@gmail.com>
2025-01-08 15:28:56 +0700

    add event-data.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import { Calculator2 } from './calculator'
+import {EventData2} from './event-data'
 
 function App() {
-  return <Calculator2/>
+  return <EventData2/>
 }
 
 export default App;
