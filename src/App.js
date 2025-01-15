a0d0b93b28198be1edb2b5b087e991395184d91a
onwaree <onwareesrimaya21082545@gmail.com>
2025-01-15 15:55:01 +0700

    add state-func

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from 'react'
-import RefsFunc from './refs-func'
+import MessageBox from './state-func'
 
 function App() {
-  return <RefsFunc/>
+  return <MessageBox/>
 }
 
 export default App;
- 
