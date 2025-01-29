b8a1c28f7249fb4d3356007ba72beb0e5ecc093d
onwaree <onwareesrimaya21082545@gmail.com>
2025-01-29 14:21:49 +0700

    add context-contents.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react'
-import MessageBox from './state-func'
+import { userContext } from './context';
+import Content from './context-contents';
 
 function App() {
-  return <MessageBox/>
+  return(
+    <userContext.Provider value={'Tom Jerry'}>
+      <Content/>
+    </userContext.Provider>
+  )
 }
 
 export default App;
